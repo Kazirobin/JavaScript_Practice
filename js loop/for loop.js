59a8aa046d75e89f3db685470cdd2adc9ee0e94f
Kazi Robin <kazirobinking@gmail.com>
2024-01-13 19:07:05 +0600

    up

--- js loop/for loop.js
+++ js loop/for loop.js
@@ -1,9 +1,12 @@
-// console.log("hello");
-// for (let i=1; i<=30;i++){
-//     if(i>10 && i<20){i+=2}
-//     if(i<10 || i>20){i+=2}
-//     console.log(i);
-// }
+console.log("hello");
+/*
+*/
+for (let i=1; i<=30;i++){
+    // if(i>10 && i<20){i+=2}
+    if(i<10 || i>20){i+=2}
+    console.log(i);
+}
+/*
 const value = 30;
 var a = [5,8,10,12,15,18,20,22,24];
 
@@ -11,3 +14,4 @@ var a = [5,8,10,12,15,18,20,22,24];
 a.forEach(e => {
     console.log(e);
 });
+*/
