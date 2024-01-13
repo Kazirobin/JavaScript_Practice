0e19ee2381eb832a25f34687294dd4cf0179751f
Kazi Robin <kazirobinking@gmail.com>
2024-01-13 19:00:36 +0600

    up

--- js loop/for.js
+++ js loop/for.js
@@ -2,26 +2,34 @@
 /*
 for (initialization ; condition ; update ){ executed code}
 */
-// for(let i=1; i<=30; i+=2){
-//     if(i>14 && i<17){continue}
-//     if(i==19){continue}
-//     if(i>24){break}
-//     console.log("this is number " + i);
-// }
-// for(let i=1; i<=30; i++){
+/*
+for(let i=1; i<=30; i+=2){
+    if(i>14 && i<17){continue}
+    if(i==19){continue}
+    if(i>24){break}
+    console.log("this is number " + i);
+}
+*/
+/*
+for(let i=1; i<=30; i++){
 
-//     if(i<10){i+=2} //if i less then 10
-//     if(i>20){i++} //if i greater then 20 
-//     console.log("this is number " + i);
-// }
-// for(let i=1; i<=30; i++){
-//     if(i<10 || i>20){i+=2} //if i less then 10 and if i greater then 20 
-//     console.log("this is number " + i);
-// }
-// for(let i=1; i<=30; i++){
-//     if(i>10 && i<20){i+=2} //if i greater  then 10 if i less then 20 
-//     console.log("this is number " + i);
-// }
+    if(i<10){i+=2} //if i less then 10
+    if(i>20){i++} //if i greater then 20 
+    console.log("this is number " + i);
+}
+*/
+/*
+for(let i=1; i<=30; i++){
+    if(i<10 || i>20){i+=2} //if i less then 10 and if i greater then 20 
+    console.log("this is number " + i);
+}
+*/
+/*
+for(let i=1; i<=30; i++){
+    if(i>10 && i<20){i+=2} //if i greater  then 10 if i less then 20 
+    console.log("this is number " + i);
+}
+*/
 // let arr = ["one","two","tree","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen"]
 var num1 = "kazi";
 var num2 = "robin"
