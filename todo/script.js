687600f6c906bcf6fd8a870b17eabd77f6de08ce
Kazi Robin <kazirobinking@gmail.com>
2024-01-26 19:03:12 +0600

    up

--- todo/script.js
+++ todo/script.js
@@ -3,7 +3,7 @@ let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
 let todoUl = document.querySelector('#items');
 let completeUl = document.querySelector('.complete-list ul');
-
+ 
 let createTask = function(task){
     let listItem = document.createElement('li');
     let checkBox = document.createElement('input');
